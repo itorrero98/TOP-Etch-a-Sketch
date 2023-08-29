410ecb1f8d391d4aba7209cd5e6f77e6287ee17f
issiac.torrero <itorrero@sdgecontractor.com>
2023-08-29 16:23:25 -0600

    Initial sketching ability implemented

---
+++ src/scripts/sketch.js
@@ -0,0 +1,23 @@
+const CONTAINER = document.getElementById('container');
+//16x16 grid
+const GRID_X_SIZE = 100; 
+const GRID_Y_SIZE = 100;
+CONTAINER.style.gridTemplateColumns = `repeat(${GRID_X_SIZE}, auto)`;
+let createGrid = () => {
+    for(let i = 0; i < (GRID_X_SIZE * GRID_Y_SIZE); i++) {
+        let cell = document.createElement('div');
+        cell.classList += 'cell';
+        CONTAINER.appendChild(cell);
+    }
+}
+
+createGrid();
+
+const GRID = document.querySelectorAll('.cell');
+
+GRID.forEach((cell) => {
+    cell.addEventListener('mouseover', (e) => {
+        e.target.classList.add('hovered');
+    })
+})
+
